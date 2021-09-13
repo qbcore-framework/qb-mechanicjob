9e85e437a3e906831c8233712413a2601669f1d0
GhzGarage <gigahertzgarage@mail.com>
2021-09-13 01:21:26 -0500

    Update SQL queries for MySQL2

--- server/main.lua
+++ server/main.lua
@@ -9,7 +9,8 @@ RegisterServerEvent('qb-vehicletuning:server:SaveVehicleProps')
 AddEventHandler('qb-vehicletuning:server:SaveVehicleProps', function(vehicleProps)
     local src = source
     if IsVehicleOwned(vehicleProps.plate) then
-        exports.oxmysql:execute('UPDATE player_vehicles SET mods=@mods WHERE plate=@plate', {['@mods'] = json.encode(vehicleProps), ['@plate'] = vehicleProps.plate})
+        exports.oxmysql:execute('UPDATE player_vehicles SET mods = ? WHERE plate = ?',
+            {json.encode(vehicleProps), vehicleProps.plate})
     end
 end)
 
@@ -18,18 +19,18 @@ AddEventHandler("vehiclemod:server:setupVehicleStatus", function(plate, engineHe
     local src = source
     local engineHealth = engineHealth ~= nil and engineHealth or 1000.0
     local bodyHealth = bodyHealth ~= nil and bodyHealth or 1000.0
-    if VehicleStatus[plate] == nil then 
+    if VehicleStatus[plate] == nil then
         if IsVehicleOwned(plate) then
             local statusInfo = GetVehicleStatus(plate)
-            if statusInfo == nil then 
-                statusInfo =  {
+            if statusInfo == nil then
+                statusInfo = {
                     ["engine"] = engineHealth,
                     ["body"] = bodyHealth,
                     ["radiator"] = Config.MaxStatusValues["radiator"],
                     ["axle"] = Config.MaxStatusValues["axle"],
                     ["brakes"] = Config.MaxStatusValues["brakes"],
                     ["clutch"] = Config.MaxStatusValues["clutch"],
-                    ["fuel"] = Config.MaxStatusValues["fuel"],
+                    ["fuel"] = Config.MaxStatusValues["fuel"]
                 }
             end
             VehicleStatus[plate] = statusInfo
@@ -42,7 +43,7 @@ AddEventHandler("vehiclemod:server:setupVehicleStatus", function(plate, engineHe
                 ["axle"] = Config.MaxStatusValues["axle"],
                 ["brakes"] = Config.MaxStatusValues["brakes"],
                 ["clutch"] = Config.MaxStatusValues["clutch"],
-                ["fuel"] = Config.MaxStatusValues["fuel"],
+                ["fuel"] = Config.MaxStatusValues["fuel"]
             }
             VehicleStatus[plate] = statusInfo
             TriggerClientEvent("vehiclemod:client:setVehicleStatus", -1, plate, statusInfo)
@@ -56,17 +57,17 @@ RegisterServerEvent('qb-vehicletuning:server:UpdateDrivingDistance')
 AddEventHandler('qb-vehicletuning:server:UpdateDrivingDistance', function(amount, plate)
     VehicleDrivingDistance[plate] = amount
     TriggerClientEvent('qb-vehicletuning:client:UpdateDrivingDistance', -1, VehicleDrivingDistance[plate], plate)
-    local result = exports.oxmysql:fetchSync('SELECT plate FROM player_vehicles WHERE plate=@plate', {['@plate'] = plate})
+    local result = exports.oxmysql:fetchSync('SELECT plate FROM player_vehicles WHERE plate = ?', {plate})
     if result[1] ~= nil then
-        exports.oxmysql:execute('UPDATE player_vehicles SET drivingdistance=@drivingdistance WHERE plate=@plate', {['@drivingdistance'] = amount, ['@plate'] = plate})
+        exports.oxmysql:execute('UPDATE player_vehicles SET drivingdistance = ? WHERE plate = ?', {amount, plate})
     end
 end)
 
 QBCore.Functions.CreateCallback('qb-vehicletuning:server:IsVehicleOwned', function(source, cb, plate)
     local retval = false
-    local result = exports.oxmysql:scalarSync('SELECT 1 from player_vehicles WHERE plate=@plate', {['@plate'] = plate})
-    if result then 
-        retval = true 
+    local result = exports.oxmysql:scalarSync('SELECT 1 from player_vehicles WHERE plate = ?', {plate})
+    if result then
+        retval = true
     end
     cb(retval)
 end)
@@ -109,7 +110,7 @@ end)
 
 RegisterServerEvent("vehiclemod:server:fixEverything")
 AddEventHandler("vehiclemod:server:fixEverything", function(plate)
-    if VehicleStatus[plate] ~= nil then 
+    if VehicleStatus[plate] ~= nil then
         for k, v in pairs(Config.MaxStatusValues) do
             VehicleStatus[plate][k] = v
         end
@@ -120,25 +121,36 @@ end)
 RegisterServerEvent("vehiclemod:server:saveStatus")
 AddEventHandler("vehiclemod:server:saveStatus", function(plate)
     if VehicleStatus[plate] ~= nil then
-        exports.oxmysql:execute('UPDATE player_vehicles SET status = @status WHERE plate = @plate', {['@status'] = json.encode(VehicleStatus[plate]), ['@plate'] = plate})
+        exports.oxmysql:execute('UPDATE player_vehicles SET status = ? WHERE plate = ?',
+            {json.encode(VehicleStatus[plate]), plate})
     end
 end)
 
 function IsVehicleOwned(plate)
-    local result = exports.oxmysql:scalarSync('SELECT 1 from player_vehicles WHERE plate=@plate', {['@plate'] = plate})
-    if result then return true else return false end
+    local result = exports.oxmysql:scalarSync('SELECT 1 from player_vehicles WHERE plate = ?', {plate})
+    if result then
+        return true
+    else
+        return false
+    end
 end
 
 function GetVehicleStatus(plate)
     local retval = nil
-    local result = exports.oxmysql:fetchSync('SELECT status FROM player_vehicles WHERE plate=@plate', {['@plate'] = plate})
+    local result = exports.oxmysql:fetchSync('SELECT status FROM player_vehicles WHERE plate = ?', {plate})
     if result[1] ~= nil then
         retval = result[1].status ~= nil and json.decode(result[1].status) or nil
     end
     return retval
 end
 
-QBCore.Commands.Add("setvehiclestatus", "Set Vehicle Status", {{name="part", help="Type The Part You Want To Edit"}, {name="amount", help="The Percentage Fixed"}}, true, function(source, args)
+QBCore.Commands.Add("setvehiclestatus", "Set Vehicle Status", {{
+    name = "part",
+    help = "Type The Part You Want To Edit"
+}, {
+    name = "amount",
+    help = "The Percentage Fixed"
+}}, true, function(source, args)
     local part = args[1]:lower()
     local level = tonumber(args[2])
     TriggerClientEvent("vehiclemod:client:setPartLevel", source, part, level)
@@ -149,10 +161,10 @@ QBCore.Functions.CreateCallback('qb-vehicletuning:server:GetAttachedVehicle', fu
 end)
 
 QBCore.Functions.CreateCallback('qb-vehicletuning:server:IsMechanicAvailable', function(source, cb)
-	local amount = 0
-	for k, v in pairs(QBCore.Functions.GetPlayers()) do
+    local amount = 0
+    for k, v in pairs(QBCore.Functions.GetPlayers()) do
         local Player = QBCore.Functions.GetPlayer(v)
-        if Player ~= nil then 
+        if Player ~= nil then
             if (Player.PlayerData.job.name == "mechanic" and Player.PlayerData.job.onduty) then
                 amount = amount + 1
             end
@@ -184,14 +196,18 @@ AddEventHandler('qb-vehicletuning:server:CheckForItems', function(part)
             Player.Functions.RemoveItem(Config.RepairCostAmount[part].item, Config.RepairCostAmount[part].costs)
 
             for i = 1, Config.RepairCostAmount[part].costs, 1 do
-                TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.RepairCostAmount[part].item], "remove")
+                TriggerClientEvent('inventory:client:ItemBox', src,
+                    QBCore.Shared.Items[Config.RepairCostAmount[part].item], "remove")
                 Citizen.Wait(500)
             end
         else
-            TriggerClientEvent('QBCore:Notify', src, "You Dont Have Enough "..QBCore.Shared.Items[Config.RepairCostAmount[part].item]["label"].." (min. "..Config.RepairCostAmount[part].costs.."x)", "error")
+            TriggerClientEvent('QBCore:Notify', src,
+                "You Dont Have Enough " .. QBCore.Shared.Items[Config.RepairCostAmount[part].item]["label"] .. " (min. " ..
+                    Config.RepairCostAmount[part].costs .. "x)", "error")
         end
     else
-        TriggerClientEvent('QBCore:Notify', src, "You Do Not Have "..QBCore.Shared.Items[Config.RepairCostAmount[part].item]["label"].." bij je!", "error")
+        TriggerClientEvent('QBCore:Notify', src, "You Do Not Have " ..
+            QBCore.Shared.Items[Config.RepairCostAmount[part].item]["label"] .. " bij je!", "error")
     end
 end)
 
@@ -206,7 +222,10 @@ function IsAuthorized(CitizenId)
     return retval
 end
 
-QBCore.Commands.Add("setmechanic", "Give Someone The Mechanic job", {{name="id", help="ID Of The Player"}}, false, function(source, args)
+QBCore.Commands.Add("setmechanic", "Give Someone The Mechanic job", {{
+    name = "id",
+    help = "ID Of The Player"
+}}, false, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
 
     if IsAuthorized(Player.PlayerData.citizenid) then
@@ -215,18 +234,23 @@ QBCore.Commands.Add("setmechanic", "Give Someone The Mechanic job", {{name="id",
             local TargetData = QBCore.Functions.GetPlayer(TargetId)
             if TargetData ~= nil then
                 TargetData.Functions.SetJob("mechanic")
-                TriggerClientEvent('QBCore:Notify', TargetData.PlayerData.source, "You Were Hired As An Autocare Employee!")
-                TriggerClientEvent('QBCore:Notify', source, "You have ("..TargetData.PlayerData.charinfo.firstname..") Hired As An Autocare Employee!")
+                TriggerClientEvent('QBCore:Notify', TargetData.PlayerData.source,
+                    "You Were Hired As An Autocare Employee!")
+                TriggerClientEvent('QBCore:Notify', source, "You have (" .. TargetData.PlayerData.charinfo.firstname ..
+                    ") Hired As An Autocare Employee!")
             end
         else
             TriggerClientEvent('QBCore:Notify', source, "You Must Provide A Player ID!")
         end
     else
-        TriggerClientEvent('QBCore:Notify', source, "You Cannot Do This!", "error") 
+        TriggerClientEvent('QBCore:Notify', source, "You Cannot Do This!", "error")
     end
 end)
 
-QBCore.Commands.Add("firemechanic", "Fire A Mechanic", {{name="id", help="ID Of The Player"}}, false, function(source, args)
+QBCore.Commands.Add("firemechanic", "Fire A Mechanic", {{
+    name = "id",
+    help = "ID Of The Player"
+}}, false, function(source, args)
     local Player = QBCore.Functions.GetPlayer(source)
 
     if IsAuthorized(Player.PlayerData.citizenid) then
@@ -236,8 +260,10 @@ QBCore.Commands.Add("firemechanic", "Fire A Mechanic", {{name="id", help="ID Of 
             if TargetData ~= nil then
                 if TargetData.PlayerData.job.name == "mechanic" then
                     TargetData.Functions.SetJob("unemployed")
-                    TriggerClientEvent('QBCore:Notify', TargetData.PlayerData.source, "You Were Fired As An Autocare Employee!")
-                    TriggerClientEvent('QBCore:Notify', source, "You have ("..TargetData.PlayerData.charinfo.firstname..") Fired As Autocare Employee!")
+                    TriggerClientEvent('QBCore:Notify', TargetData.PlayerData.source,
+                        "You Were Fired As An Autocare Employee!")
+                    TriggerClientEvent('QBCore:Notify', source,
+                        "You have (" .. TargetData.PlayerData.charinfo.firstname .. ") Fired As Autocare Employee!")
                 else
                     TriggerClientEvent('QBCore:Notify', source, "Youre Not An Employee of Autocare!", "error")
                 end

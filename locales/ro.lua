e2c92531212b8b40d82848ff44e836bed6a91c36
Martin Riggs <63644849+wanderrer@users.noreply.github.com>
2022-08-24 16:43:14 +0300

    Update ro.lua
    Update to match the new "template" keep in mind, no %{values} were specified in the translation so it won be accurate.

--- locales/ro.lua
+++ locales/ro.lua
@@ -1,85 +1,67 @@
---[[
-Romanian base language translation for qb-mechanicjob
-Translation done by wanderrer (Martin Riggs#0807 on Discord)
-]]--
 local Translations = {
-    progress = {
-        repveh = "Se repara...",
-    },
-    comm = {
-        svstat = "Seteaza statutul unei piese",
-        svstatp = "Piesa",
-        svstatptxt = "Numele piesei pe care vrei sa o modifici",
-        svstatpam = "Procent",
-        svstatpamtxt = "Procentajul de reparare, Ex: 30",
-        smch = "Oferi unui cetatean job-ul de mecanic auto",
-        smchid = "ID",
-        smchidtxt = "ID-ul persoanei pe care vrei sa o faci mecanic",
-        famech = "Concediaza un mecanic",
-        famechid = "ID",
-        famechidtxt = "ID-ul persoanei pe care vrei sa o concediezi",
-    },
-    notify = {
-        repcancel = "Reparatiile au fost anulate",
-        pcanceled = "Procesul a fost anulat",
-        nemat = "Nu ai suficiente materiale in depozit",
-        partrep = "Piesa %{value} a fost reparata!",
-        sunk = "Statut necunoscut",
-        navveh = "Nu este un vehicul valid",
-        navpart = "Nu este o piesa valida",
-        yancveh = "Nu esti suficient de aproape de vehicul",
-        ymbiaveh = "Trebuie sa fi la volanul vehiculului",
-        ymboveh = "Nu trebuie sa fi in vehicul, da-te jos!",
-        yantdob = "Hmm, ori nu esti la volan ori esti pe o bicicleta",
-        yanoveh = "Nu esti intr-un vehicul",
-        ydnhenough = "Nu ai suficient %{value1} (min. %{value2} x)",
-        ydnhave = "Nu ai  %{value} deloc, cumpara!",
-        ywhamech = "Ai fost angajat ca mecanic auto!",
-        yhhamech = "Ai un nou angajat, %{value} ca mecanic auto!",
-        ympavid = "Trebuie sa precizezi ID-ul cetateanului!",
-        ycndt = "Din pacate, nu poti face treaba asta!",
-        ywfasmech = "Ai fost concediat, nu mai esti mecanic auto!",
-        yhfamech = "Ai concediat angajatul %{value} nu mai este mecanic auto!",
-        yanaemmech = "Hmm, se pare ca nu esti mecanic auto!",
-
-    },
-    menu = {
-        stjob = "Incepi tura",
-        stpjob = "Iesi din tura",
-        opstash = "Deschide depozitul",
-        mheader = "Optiuni vehicul",
-        disveh = "Deconecteaza vehiculul",
-        disvehtxt = "Detaseaza vehiculul de pe cric",
-        cvech = "Check Status",
-        cvechtxt = "Verifica starea vehiculului",
-        vehparts = "Piesele vehiculului",
-        vehpartstxt = "Repara piesele vehiculului",
-        closemenu = "⬅ Inchide",
-        backmenu = "⬅ Inapoi la meniu",
-        backmenutxt = "Inapoi la piese",
-        repveh = "Repara : ",
-        nodamage = "Fara probleme",
-        pnodamage = "Inapoi",
-        pnodamagetxt = "Componenta nu are probleme!",
+    labels = {
+        engine = 'Motor',
+        bodsy = 'Caroserie',
+        radiator = 'Radiator',
+        axle = 'Cutie de viteze',
+        brakes = 'Sistem de franare',
+        clutch = 'Ambreiaj',
+        fuel = 'Rezervor',
+        sign_in = 'Intra in tura',
+        sign_off = 'Iesi din tura',
+        o_stash = '[E] Deschide depozit',
+        h_vehicle = '[E] Parcheaza vehicul',
+        g_vehicle = '[E] Foloseste vehicul',
+        o_menu = '[E] Deschide meniul',
+        work_v = '[E] Lucreaza la vehicul'
     },
 
-    interaction = {
-        pstash = "[E] Deschide depozitul",
-        hvehicle = "[E] Parcheaza vehicul",
-        gvehicle = "[E] Foloseste vehicul",
-        opmenu = "[E] Deschide meniul",
-        wovehicle = "[E] Lucreaza la vehicul",
-    },
-    label = {
-        vstatus = "Starea vehiculului",
-        mstatus = "Stare",
-        mstat2 = "Stare: ",
-        vehlist = "Lista vehicule",
-        vehtxt = "Vehicul: ",
-        ablip = "Mecanic auto", -- name of the blip
+    lift_menu = {
+        header_menu = 'Optiuni vehicul',
+        header_vehdc = 'Deconecteaza vehicul',
+        desc_vehdc = 'Scoate vehiculul de pe lift',
+        header_stats = 'Verifica statut',
+        desc_stats = 'Verifica vehiculul',
+        header_parts = 'Verifica componentele',
+        desc_parts = 'Repara componente',
+        c_menu = '⬅ Inchide meniul'
     },
 
+    parts_menu = {
+        status = 'Statut: ',
+        menu_header = 'Meniu componente',
+        repair_op = 'Repara:',
+        b_menu = '⬅ Inapoi',
+        d_menu = 'Inapoi la componente',
+        c_menu = '⬅ Inchide meniul'
+    },
+
+    nodamage_menu = {
+        header = 'No Damage',
+        bh_menu = 'Back Menu',
+        bd_menu = 'There Is No Damage To This Part!',
+        c_menu = '⬅ Close Menu'
+    },
+
+    notifications = {
+        not_enough = 'Nu ai suficient',
+        not_have = 'Nu ai',
+        not_materials = 'Nu ai suficiente materiale in depozit',
+        rep_canceled = 'Reparare anulata',
+        repaired = 'a fost reparat!',
+        uknown = 'Statut necunoscut',
+        not_valid = 'Nu este un vehicul valid',
+        not_close = 'Nu esti suficient de aproape de vehicul',
+        veh_first = 'Trebuie sa fi in vehicul mai intai',
+        outside = 'Trebuie sa fi inafara vehiculului',
+        wrong_seat = 'Nu esti sofer, sau esti pe o bicicleta',
+        not_vehicle = 'Nu esti intr-un vehicul',
+        progress_bar = 'Se repara vehiculul...',
+        process_canceled = 'Procesul a fost anulat',
+        not_part = 'Nu este un part valid'
+    }
 }
+
 Lang = Locale:new({
     phrases = Translations,
     warnOnMissing = true

56c6c0c0c60f5edd2992a9926f354f35d216324a
Jean Carlos Nunez <jeancarlosn2008@gmail.com>
2022-10-01 15:02:01 -0500

    change Locale:new

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,22 +6,22 @@ version '2.0.1'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
-	'config.lua',
-	'locales/en.lua',
+    'config.lua',
+    'locales/en.lua',
     'locales/*.lua'
 }
 
 client_scripts {
     'client/main.lua',
-	'client/drivingdistance.lua',
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/CircleZone.lua',
+    'client/drivingdistance.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/CircleZone.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 lua54 'yes'
